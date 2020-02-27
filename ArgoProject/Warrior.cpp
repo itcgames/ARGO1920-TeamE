2decb56b9331c26487be0c35d8233ebe158df857
PaulBNolan <C00213034@itcarlow.ie>
2020-02-27 15:03:11 +0000

    The floor is lava

--- ArgoProject/Warrior.cpp
+++ ArgoProject/Warrior.cpp
@@ -146,7 +146,7 @@ void Warrior::update()
 		}
 	}
 
-	if (commandQueue.empty() && !m_ih->move && m_animationRect->x >= 1400 || !commandQueue.empty() && m_animationRect->x >= 1400 && !m_ih->move)
+	if ((commandQueue.empty() && !m_ih->move && m_animationRect->x >= 1400) || (!commandQueue.empty() && m_animationRect->x >= 1400 && !m_ih->move) || m_mc->getMana() <= 0)
 	{
 		finiteStateMachine->idle();
 	}
