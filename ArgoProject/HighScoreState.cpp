70e7384f248f7f1e8cfbd5746f2ab5b03e7d90be
Alex OToole <tooler1999@gmail.com>
2020-02-28 09:27:11 +0000

    highscore change

--- ArgoProject/HighScoreState.cpp
+++ ArgoProject/HighScoreState.cpp
@@ -65,7 +65,13 @@ bool HighScoreState::onEnter()
 		m_scoreString[i] = std::to_string(m_score[i]);
 	}
 
-	long int temp = 0;
+
+	SDL_Color White = { 255, 255, 255 };
+
+
+	SDL_Surface* surfaceSlot = IMG_Load("Assets/Button.png");
+	m_slotTexture = SDL_CreateTextureFromSurface(Render::Instance()->getRenderer(), surfaceSlot);
+	int temp = 0;
 	std::string nameTemp = "";
 	for (int i = 0; i < 10; i++)
 	{
@@ -84,18 +90,8 @@ bool HighScoreState::onEnter()
 			}
 		}
 		m_display[i] = std::string(std::to_string(i + 1) + ". " + m_names[i] + " - " + m_scoreString[i]);
-	}
-
-	SDL_Color White = { 255, 255, 255 };
-
-
-	SDL_Surface* surfaceSlot = IMG_Load("Assets/Button.png");
-	m_slotTexture = SDL_CreateTextureFromSurface(Render::Instance()->getRenderer(), surfaceSlot);
-
-	for (int i = 0; i < 10; i++)
-	{
 		m_text[i] = new Text(Abel, m_display[i], m_cameraDimensions.x * 0.2, m_cameraDimensions.y * ((i * 0.1)));
-
+		m_text[i]->update(m_display[i]);
 		m_slotRect[i] = new SDL_Rect();
 
 		m_slotRect[i]->x = m_cameraDimensions.x * 0.2;
@@ -104,7 +100,7 @@ bool HighScoreState::onEnter()
 		m_slotRect[i]->h = m_cameraDimensions.y * 0.1 - 10;
 	}
 	SDL_FreeSurface(surfaceSlot);
-
+	
 	m_exitOption = new SDL_Rect();
 	m_exitOption->x = m_cameraDimensions.x * (2.5 / 6.0);
 	m_exitOption->y = m_cameraDimensions.y * 0.9;
